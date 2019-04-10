985f44ea0995bdf92f5246b71fb40002492652e8
pangxiao <pangxiao@360.cn>
2019-04-10 19:42:56 +0800

    修改Slider2

--- slider/Slider2.jsx
+++ slider/Slider2.jsx
@@ -1,81 +1,80 @@
-// http://laichuanfeng.com/work/carousel-in-react/
 import React, { Component } from 'react'
 
-class Carousel extends Component {
-    state = { ptr: 0 }
-    timeout = null//轮播定时器
-    static url = 'http://laichuanfeng.com/demo/carousel/'
-    static imgs = [
-        'carousel_1.jpg',
-        'carousel_2.jpg',
-        'carousel_3.jpg',
-        'carousel_4.jpg',
-        'carousel_5.jpg'
-    ]
-
-    /**
-     * 循环输出索引
-     * @param startIndex {Number} 开始的索引
-     * @param interval {Number} 间隔
-     * @param length {Number} 循环长度
-     */
-    static makeIndex = ({ startIndex, interval, length }) => {
-        let index = startIndex
-        return () => {
-            index += interval
-            return index % length
+export default class Slider extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            currentIndex: 0,
+            orientation: 1 // 轮播图滚动方向，默认正向，为1，反向是-1
         }
     }
 
-    /**
-     * 定时器，切换指针指向
-     * @param startIndex {Number} 轮播开始时的元素索引
-     */
-    cyclePlay = startIndex => {
-        const index = Carousel.makeIndex({
-            startIndex,
-            interval: 1,
-            length: Carousel.imgs.length
+    handleChangeIndex(orientation) {
+        const length = this.props.children.length
+        const { currentIndex } = this.state
+        this.setState({
+            currentIndex: (currentIndex + orientation + length) % length,
+            orientation
         })
-        const interval = 3000
-        this.setState({ ptr: startIndex })
-
-        if (this.timeout) clearTimeout(this.timeout)
-        this.timeout = setTimeout(function timer() {
-            this.setState({ ptr: index() })
-            this.timeout = setTimeout(timer.bind(this), interval)
-        }.bind(this), interval)
-
     }
 
-    /**
-     * 计算当前元素的样式
-     * @param index {Number} 元素的索引
-     */
-    getClassName = index => {
-        const { ptr } = this.state
-        const length = Carousel.imgs.length
-        return index === ptr
-            ? 'center'
-            : index === (ptr + length - 1) % length
-                ? 'left'
-                : 'right'
-    }
-
-    componentDidMount() {
-        this.cyclePlay(0)
+    renderChildren() {
+        const { children, width = 100 } = this.props
+        const { currentIndex, orientation } = this.state
+        const length = children.length
+        // 这里需要特别注意， center、left、right 切换时，需要考虑动画的 z-index
+        let itemStyles = {}
+        if (orientation === 1) {
+            itemStyles = {
+                center: { position: 'absolute', width, left: 0, transition: 'left 0.5s ease-in-out', zIndex: 1 },
+                left: { position: 'absolute', width, left: -width, transition: 'left 0.5s ease-in-out' },
+                right: { position: 'absolute', width, left: width, transition: 'left 0.5s ease-in-out', zIndex: -1 }
+            }
+        } else if (orientation === -1) {
+            itemStyles = {
+                center: { position: 'absolute', width, left: 0, transition: 'left 0.5s ease-in-out', zIndex: 1 },
+                left: { position: 'absolute', width, left: -width, transition: 'left 0.5s ease-in-out' },
+                right: { position: 'absolute', width, left: width, transition: 'left 0.5s ease-in-out', zIndex: 1 }
+            }
+        }
+        return children.map((child, index) => {
+            let style = {}
+            if (currentIndex === index) {
+                style = itemStyles.center
+            } else if (index === (currentIndex + length - 1) % length) {
+                style = itemStyles.left
+            } else {
+                style = itemStyles.right
+            }
+            return React.cloneElement(child, { key: index, style })
+        })
     }
 
     render() {
-        return (
-            <div className='content'>
-                <ul className='carousel'>
-                    {
-                        Carousel.imgs.map((img, index) => <li key={index} className={this.getClassName(index)}
-                                                              style={{ background: `url(${Carousel.url}${img})` }}></li>)
-                    }
-                </ul>
-            </div>
-        )
-    };
+        const { width = 200, height = 100 } = this.props
+
+        const parentStyle = {
+            width,
+            height,
+            position: 'relative',
+            overflow: 'hidden'
+        }
+
+        let containerStyle = {
+            width,
+            height,
+            position: 'relative'
+        }
+
+        return <div style={parentStyle}>
+            <div style={containerStyle}>{this.renderChildren()}</div>
+            <button onClick={() => this.handleChangeIndex(-1)}
+                    style={{ position: 'absolute', left: 0, top: '50%', height: '30px', marginTop: '-15px', zIndex: 10 }}>上一张
+            </button>
+            <button onClick={() => this.handleChangeIndex(1)}
+                    style={{ position: 'absolute', right: 0, top: '50%', height: '30px', marginTop: '-15px', zIndex: 10 }}>下一张
+            </button>
+        </div>
+    }
 }
+
